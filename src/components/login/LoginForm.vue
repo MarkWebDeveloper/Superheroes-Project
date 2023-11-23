209fed768504ea5932e2e4ec89c50d5926b35d17
Brusmel Gil <Brusmel@outlook.es>
2023-11-23 13:00:02 +0100

    base

--- src/components/login/LoginForm.vue
+++ src/components/login/LoginForm.vue
@@ -4,13 +4,44 @@
 
 <template>
     
-<form>
-    <h1>This is login form</h1>
-    <img src="../../../public/images/background/background-pic-login.webp" alt="">
-</form>
+<section id="form">
+
+<div class="form-body">
+
+    <p class="text-form">LOGIN SESSION </p> 
+
+            <form>
+                    <input type="email" name="email" placeholder="Email">
+                    <input type="password" name="password" placeholder="Password" >
+                <a href="#">REGISTER HERE</a>
+
+                    <button>SING IN</button>
+
+                    </form>
+
+
+    </div>
+
+</section>
+
 
 </template>
 
 <style scoped>
+
+#form{
+    background-image: url("/images/background/background-pic-login.webp");
+    
+}
+
+.form-body{
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+}
+
+
+
+
 
 </style>
