bc6b42396bd8c717a2ffc9f84452a17a808c0229
Dani Gallo <danigallo@Dani.local>
2023-11-23 12:19:46 +0100

    card container

---
+++ src/components/Home/cards/CardFlex.vue
@@ -0,0 +1,28 @@
+<script setup>
+
+</script>
+<template>
+  <main class="container text-center d-flex flex-wrap">
+      <div class="CardContainer">
+         <div class="ShPhoto" src="./images/logo/logo-superheroes.png">
+      </div>
+      <div class="Atributes">
+         <h1 class="ShName"></h1>
+         <p class="ShIntelligence">INTELLIGENCE</p>
+         <p class="Power">POWER</p>
+         <p class="Speed">SPEED</p>
+     </div>
+    </div>
+  </main>
+</template>
+<style>
+main{
+    display: flex;
+    flex-wrap: nowrap;
+    background-color: aqua;
+    height: 100%;
+  }
+  .shPhoto{
+    background: cover;
+  }
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,11 @@
 <script setup>
-
+import CardFlex from '../components/Home/cards/CardFlex.vue';
 </script>
 
 <template>
   <main>
     <h1>This is the home page</h1>
+    <CardFlex/>
   </main>
 </template>
 
