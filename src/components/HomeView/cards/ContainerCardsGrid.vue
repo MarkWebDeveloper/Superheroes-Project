417a92e45eacf8163984e049ceac4013871e6020
IsabelAM <isa_am_@hotmail.com>
2023-11-27 12:16:00 +0100

    add container

--- src/components/HomeView/cards/ContainerCardsGrid.vue
+++ src/components/HomeView/cards/ContainerCardsGrid.vue
@@ -3,106 +3,48 @@
 </script>
 
 <template>
-    <div class="container">
-        <div class="card1"></div>
-        <div class="card2"></div>
-        <div class="card3"></div>
-        <div class="card4"></div>
-        <div class="card5"></div>
-        <div class="card6"></div>
-        <div class="card7"></div>
-        <div class="card8"></div>
-        <div class="card9"></div>
-        <div class="card10"></div>
+    <div id ="container">
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
         </div>
 </template>
 
 <style scoped lang="scss">
 
-.container {
+
+
+
+#container {
     display: grid;
-    height: 70%;
-    width: auto;
-    grid-template-rows: 1fr 1fr 1fr 1fr 1fr;
-    grid-template-columns: 1fr 1fr;
-    gap: 2% 2%;
-    grid-auto-flow: row;
-    justify-items: center;
-    grid-template-areas:
-    "area1 area2 area3 area4 area5"
-    "area6 area7 area8 area9 area10";
+    grid-template-columns: repeat(2, 1fr);
+    grid-gap: 20px;
 }
 
-    .card1 {
+    .card {
         background: green;
-        background-size: 100% 100%; 
+        padding: 1rem;
         border: rgb(2, 0, 0) 2px solid;
-        grid-area: area1;
     }
 
-    .card2 {
-        background: red;
-        background-size: 100% 100%; 
-        border: rgb(2, 0, 0) 2px solid;
-        grid-area: area2;
+    /* Móviles en vertical */
+    /*añadir a media, las cosas q quiero cambiar solamente, ej:ancho contenedor, no las cajitas*/
+
+/*@media (min-width: 768px) {
+}*/
+
+@media only screen and (min-width: 768px) {
+    #container {
+        display: grid;
+        grid-template-columns: repeat(5, 1fr);
     }
-
-    .card3 {
-        background: purple;
-        background-size: 100% 100%; 
-        border: rgb(2, 0, 0) 2px solid;
-        grid-area: area3;
-    }
-
-    .card4 {
-        background: blue;
-        background-size: 100% 100%; 
-        border: rgb(2, 0, 0) 2px solid;
-        grid-area: area4;
-    }
-
-    .card5 {
-        background: powderblue;
-        background-size: 100% 100%; 
-        border: rgb(2, 0, 0) 2px solid;
-        grid-area: area5;
-    }
-
-    .card6 {
-        background: turquoise;
-        background-size: 100% 100%; 
-        border: rgb(2, 0, 0) 2px solid;
-        grid-area: area6;
-    }
-
-    .card7 {
-        background: azure;
-        background-size: 100% 100%; 
-        border: rgb(2, 0, 0) 2px solid;
-        grid-area: area7;
-    }
-
-    .card8 {
-        background: greenyellow;
-        background-size: 100% 100%; 
-        border: rgb(2, 0, 0) 2px solid;
-        grid-area: area8;
-    }
-
-    .card9 {
-        background: blueviolet;
-        background-size: 100% 100%; 
-        border: rgb(2, 0, 0) 2px solid;
-        grid-area: area9;
-    }
-
-    .card10 {
-        background: yellow;
-        background-size: 100% 100%; 
-        border: rgb(2, 0, 0) 2px solid;
-        grid-area: area10;
-    }
-
-
+}
 
 </style>
